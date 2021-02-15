9be4f2360ccef26e09f59cb4097a1fe6add39879
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-02-15 21:48:42 +0900

    feat : init code for drawing right triangle with 'for loop'

---
+++ src/rightTriangle/main.java
@@ -0,0 +1,15 @@
+package rightTriangle;
+
+public class main {
+	public static void main(String[] args) {
+		 ShapePrinter printer = new ShapePrinter();
+
+	        // Å×½ºÆ®
+	        printer.printTriangle(3);
+	        System.out.println("----------");
+	        printer.printTriangle(5);
+	        System.out.println("----------");
+	        printer.printTriangle(10);		
+	}
+
+}
